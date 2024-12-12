fe51119ff388b7ac43a0eeab8c5da4e502612e22
Sherzod <omilxanovs7@gmail.com>
2024-12-12 19:01:49 +0500

    Changed

--- src/components/ourAwesomeBenefits/ourAwesomeBenefitsCard.jsx
+++ src/components/ourAwesomeBenefits/ourAwesomeBenefitsCard.jsx
@@ -5,7 +5,7 @@ const OurAwesomeBenefitsCard = ({card}) => {
 	return (
 		<div className='card'>
 			<div className="card-image">
-				<img src="../../../public/Method.png" alt="" />
+				<img src="/Method.png" alt="" />
 			</div>
 			<div className="card-text">
 				<h2>{card.title}</h2>
